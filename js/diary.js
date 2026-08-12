2cd483828e4bb87d9d00d8b21f058c2c88aba3a2
Simon Armstrong <simon.armstrong.medium@hotmail.com>
2026-08-12 11:05:23 +0100

    Add September and October 2026 diary dates

--- js/diary.js
+++ js/diary.js
@@ -2,6 +2,18 @@ const diary = [
   {date:"2026-08-08",time:"7:00pm",venue:"The Circle @ Greenside",location:"Greenside Community Centre, NE40 4AA",type:"Open Circle",status:"upcoming"},
   {date:"2026-08-12",time:"7:15pm",venue:"Philadelphia SNU Spiritualist Church",location:"",type:"Night of Mediumship",status:"upcoming"},
   {date:"2026-08-26",time:"7:00pm",venue:"Chichester Christian Spiritualist Church",location:"South Shields",type:"Night of Mediumship",status:"upcoming"},
-  {date:"2026-08-30",time:"6:00pm",venue:"Horden SNU Spiritualist Church",location:"",type:"Divine Service",status:"upcoming"}
+  {date:"2026-08-30",time:"6:00pm",venue:"Horden SNU Spiritualist Church",location:"",type:"Divine Service",status:"upcoming"},
+
+  {date:"2026-09-05",time:"7:00pm",venue:"The Circle @ Greenside",location:"Greenside Community Centre, NE40 4AA",type:"Open Circle",status:"upcoming"},
+  {date:"2026-09-09",time:"7:00pm",venue:"Jarrow Spiritualist Church",location:"Jarrow",type:"Divine Service",status:"upcoming"},
+  {date:"2026-09-16",time:"7:30pm",venue:"Elite Christian Spiritualist Church",location:"",type:"Service",status:"upcoming"},
+  {date:"2026-09-27",time:"6:00pm",venue:"Craghead Spiritualist Church",location:"Craghead",type:"Divine Service",status:"upcoming"},
+  {date:"2026-09-29",time:"7:00pm",venue:"Willington Spiritualist Church",location:"Willington",type:"Night of Mediumship",status:"upcoming"},
+
+  {date:"2026-10-03",time:"7:00pm",venue:"The Circle @ Greenside",location:"Greenside Community Centre, NE40 4AA",type:"Open Circle",status:"upcoming"},
+  {date:"2026-10-05",time:"7:00pm",venue:"Heaton and Byker Spiritualist Church",location:"Newcastle upon Tyne",type:"Service",status:"upcoming"},
+  {date:"2026-10-12",time:"7:30pm",venue:"Birtley Spiritualist Church",location:"Birtley",type:"Divine Service",status:"upcoming"},
+  {date:"2026-10-18",time:"6:00pm",venue:"Cramlington SNU Spiritualist Church",location:"Cramlington",type:"Divine Service",status:"upcoming"},
+  {date:"2026-10-31",time:"7:00pm",venue:"Gateshead & Whickham Spiritualist Centre",location:"Gateshead",type:"Halloween Special",status:"upcoming"}
 ];
 window.diary = diary;
