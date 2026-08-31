c1ef172f4fc6fd7ee021a45c75c477775c21cc86
Simon Armstrong <simon.armstrong.medium@hotmail.com>
2026-08-31 08:11:06 +0100

    Add November Open Circle

--- js/diary.js
+++ js/diary.js
@@ -21,6 +21,7 @@ const diary = [
   {date:"2026-11-18",time:"7:00pm",venue:"Chichester Christian Spiritualist Church",location:"South Shields",type:"Night of Mediumship",status:"upcoming"},
   {date:"2026-11-22",time:"6:30pm",venue:"Heaton and Byker Spiritualist Church",location:"Newcastle upon Tyne",type:"Divine Service",status:"upcoming"},
   {date:"2026-11-26",time:"7:30pm",venue:"Beacon of Light Spiritualist Church",location:"",type:"Night of Mediumship",status:"upcoming"},
+  {date:"2026-11-28",time:"7:00pm",venue:"The Circle @ Greenside",location:"Greenside Community Centre, NE40 4AA",type:"Open Circle",status:"upcoming"},
   {date:"2026-11-29",time:"7:00pm",venue:"Hartlepool Spiritualist Church",location:"Masonic Hall",type:"Divine Service",status:"upcoming"}
 ];
 window.diary = diary;
